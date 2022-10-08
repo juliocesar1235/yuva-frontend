17cdfd0a86d6499214fb99930fcce8b9c5aa395f
Karina Amador <akarina.at@gmail.com>
2022-10-08 14:58:06 -0500

    arreglé ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PerfilContratistaComponent } from './perfil-contratista/perfil-contrati
 
 const routes: Routes = [
   {
-    path: 'perfil-contratista/',
+    path: 'perfil-contratista',
     component: PerfilContratistaComponent
   }
 ];
