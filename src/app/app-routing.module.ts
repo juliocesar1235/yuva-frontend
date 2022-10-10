2a7d7629fb9e7108beee3e83c71c76afb79b58ec
Karina Amador <akarina.at@gmail.com>
2022-10-10 15:08:51 -0500

    mover componentes a carpeta componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { PerfilContratistaComponent } from './perfil-contratista/perfil-contratista.component';
-import { PerfilEmpleadoComponent } from './perfil-empleado/perfil-empleado.component';
+import { PerfilContratistaComponent } from './components/perfil-contratista/perfil-contratista.component';
+import { PerfilEmpleadoComponent } from './components/perfil-empleado/perfil-empleado.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'prefix' },
-  {
-    path: 'perfil-contratista',
-    component: PerfilContratistaComponent
-  },
-  {
-    path: 'perfil-empleado',
-    component: PerfilEmpleadoComponent
-  }
+
+  { path: 'perfil-contratista', component: PerfilContratistaComponent },
+  { path: 'perfil-empleado', component: PerfilEmpleadoComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'prefix' }
 ];
 @NgModule({
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PerfilContratistaComponent } from './perfil-contratista/perfil-contratista.component';
+import { PerfilContratistaComponent } from './components/perfil-contratista/perfil-contratista.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { PerfilEmpleadoComponent } from './perfil-empleado/perfil-empleado.component';
+import { PerfilEmpleadoComponent } from './components/perfil-empleado/perfil-empleado.component';
 
 
 @NgModule({
