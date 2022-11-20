7db9ec1c5829b99cb6594bc910a44c62d8249115
Paola Rijo <masiel.107@gmail.com>
2022-11-20 14:13:58 -0600

    Code done getting http error becuase I'm missing data from the previous page (google login)

--- src/app/components/signup-employee/signup-employee.component.ts
+++ src/app/components/signup-employee/signup-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SignupEmployeeService } from '../../services/signup-employee.service';
 
 @Component({
   selector: 'app-signup-employee',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupEmployeeComponent implements OnInit {
 
-  constructor() { }
+  public pservices: Array<any> = [];
+
+  constructor(private signupemployeeservices:SignupEmployeeService) { 
+    this.signupemployeeservices.getServices().subscribe((resp: any)=>{
+      this.pservices = resp;
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  getUserFormData(data:{firstName: string, lastName: string, dateOfBirth: string, serviceCategory: string, address: string, city: string, zipcode: string, phoneNumber: string}){
+    console.log(data)
+    this.signupemployeeservices.serveUser(data).subscribe((result)=>{
+      console.warn(result)
+    })
   }
 
 }
