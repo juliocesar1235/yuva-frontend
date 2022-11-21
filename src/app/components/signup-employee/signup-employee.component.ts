df1352be70cdf6d4c31b7154312d22cbcc4c6b64
Paola Rijo <masiel.107@gmail.com>
2022-11-20 20:54:02 -0600

    It works correctly. After google authentification is finished the hardcoded parts need to be changed

--- src/app/components/signup-employee/signup-employee.component.ts
+++ src/app/components/signup-employee/signup-employee.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 import { SignupEmployeeService } from '../../services/signup-employee.service';
+
+interface EmployeeI{
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  dateOfBirth: string;
+  avatar: string;
+  address: string;
+  zipcode: string;
+  city: string;
+  country: string;
+  userType: string;
+  serviceCategory: string;
+}
+
 
 @Component({
   selector: 'app-signup-employee',
   templateUrl: './signup-employee.component.html',
   styleUrls: ['./signup-employee.component.scss']
 })
+
 export class SignupEmployeeComponent implements OnInit {
 
   public pservices: Array<any> = [];
@@ -18,10 +36,31 @@ export class SignupEmployeeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  selectedService = '';
+	onSelected(value:string):void {
+		this.selectedService = value;
+	}
 
-  getUserFormData(data:{firstName: string, lastName: string, dateOfBirth: string, serviceCategory: string, address: string, city: string, zipcode: string, phoneNumber: string}){
-    console.log(data)
-    this.signupemployeeservices.serveUser(data).subscribe((result)=>{
+  getUserFormData(data:{firstName: string, lastName: string, dob: NgbDate, serviceCategory: string, address: string, city: string, zipcode: string, phoneNumber: string}){
+    
+    let newData:EmployeeI = { firstName: data.firstName,
+      lastName: data.lastName,
+      email: "fea@gmail.com",
+      phoneNumber: data.phoneNumber,
+      dateOfBirth: new Date(data.dob.year, data.dob.month - 1, data.dob.day).toISOString(),
+      avatar: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.wikipedia.org%2Fwiki%2FPancho_Villa&psig=AOvVaw0O6piA2d_Psypb5HjvHrNx&ust=1665694588842000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKDCm4TK2_oCFQAAAAAdAAAAABAD",
+      address: data.address,
+      zipcode: data.zipcode,
+      city: data.city,
+      country: "Mexico",
+      userType: "employee",
+      serviceCategory: this.selectedService
+    }
+
+    console.log(newData)
+    
+    this.signupemployeeservices.serveUser(newData).subscribe((result)=>{
       console.warn(result)
     })
   }
