4b6142ebec264ed65d6c2e3ca784075294efbefa
Paola Rijo <masiel.107@gmail.com>
2022-11-28 20:44:18 -0600

    Finished login functionality

---
+++ src/app/services/google-login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleLoginService {
+  url2 = 'http://localhost:3000/yuva-api/users/login/'
+
+  constructor(private http: HttpClient) { }
+
+  serveUser(email:string){
+    console.log("HEREEEEE")
+    let header = new HttpHeaders().set('Type-content', 'application/json')
+    return this.http.get(this.url2 + email, {
+      headers: header
+    });
+  }
+}
