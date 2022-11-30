d4ff1d2ea8c341551cb1efa4d16ad98ebab3c504
Karina Amador <akarina.at@gmail.com>
2022-11-30 14:16:14 -0600

    getting user

--- src/app/components/service-list/service-list.component.ts
+++ src/app/components/service-list/service-list.component.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 // import {GoogleSigninService } from '../../services/google-signin.service';
 import { GoogleLoginService } from 'src/app/services/google-login.service';
 import { IUserRes } from 'src/app/interfaces/user';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-service-list',
@@ -20,8 +21,10 @@ import { IUserRes } from 'src/app/interfaces/user';
 export class ServiceListComponent implements OnInit {
   public services: Array<any> = [];
 
-  user!: SocialUser;
+  socialUser!: SocialUser;
+  user:any;
   isLoggedin?: boolean = false
+ 
 
   constructor(
     private servicelistservices:ServiceListService,
@@ -29,7 +32,8 @@ export class ServiceListComponent implements OnInit {
     private socialAuthService: SocialAuthService,
     public activatedRoute: ActivatedRoute,
     private loginS: GoogleLoginService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
     ) { 
     this.servicelistservices.getServices().subscribe((resp: any)=>{
       // console.log(resp)
@@ -49,10 +53,18 @@ export class ServiceListComponent implements OnInit {
 // lUgo hacer el put del usuario actualizado
   }
 
+  loadUser(){
+    this.http
+    .get('http://localhost:3000/yuva-api/users/' + this.socialUser.id)
+    .subscribe((response) => {
+      this.user = response
+    })
+  }
+
   ngOnInit(): void {
 
     this.socialAuthService.authState.subscribe((user) => {
-      this.user = user;
+      this.socialUser = user;
       //637ae8121e9375a11cd42ea6
       this.isLoggedin = (user != null);
 
@@ -67,7 +79,8 @@ export class ServiceListComponent implements OnInit {
       // }) 
 
       // this.user
-      console.log(this.user)
+      console.log(this.socialUser)
+   
     });
   }
 
